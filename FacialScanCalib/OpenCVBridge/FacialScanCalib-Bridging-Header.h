3df305bbfd78ec6026d20baf51cc3dcf1d34f02e
kgl980304-design <kgl980304@gmail.com>
2026-08-03 20:00:07 +0900

    Delete FacialScanCalib directory

--- FacialScanCalib/OpenCVBridge/FacialScanCalib-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-#ifndef FacialScanCalib_Bridging_Header_h
-#define FacialScanCalib_Bridging_Header_h
-
-// 주의: opencv2/opencv.hpp를 이 헤더에서 직접 import하지 마세요.
-// Swift 브리징 헤더는 순수 C/Objective-C 헤더만 안전하게 처리하므로,
-// OpenCV의 C++ 심볼은 OpenCVWrapper.mm 내부(구현부)에서만 include 합니다.
-#import "OpenCVWrapper.h"
-
-#endif /* FacialScanCalib_Bridging_Header_h */
