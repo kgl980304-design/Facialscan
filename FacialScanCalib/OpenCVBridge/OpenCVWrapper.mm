8cb5801b1a1f406bd703d45cac54a67aa8eb2a84
kgl980304-design <kgl980304@gmail.com>
2026-08-03 19:00:56 +0900

    Update OpenCVWrapper.mm

--- FacialScanCalib/OpenCVBridge/OpenCVWrapper.mm
+++ FacialScanCalib/OpenCVBridge/OpenCVWrapper.mm
@@ -1,9 +1,9 @@
-#import "OpenCVWrapper.h"
-
 #ifdef __cplusplus
 #import <opencv2/opencv.hpp>
 #import <opencv2/imgcodecs/ios.h>
 #endif
+
+#import "OpenCVWrapper.h"
 
 @implementation ChessboardDetection {
     BOOL _found;
